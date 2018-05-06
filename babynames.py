3cd8803807765beac77425e6373239650f9af005
Dayalan <arasan_dt@yahoo.co.in>
2018-05-06 18:57:22 +0530

    matched the code with solution to understand what was missed.

--- babynames.py
+++ babynames.py
@@ -51,27 +51,35 @@ def extract_names(f):
     sp5=[]
     soup = BS(open(f),"html5lib")
     header = soup.find_all('th')
-    para = []
+    #para = []
+    para = [str(x) for x in header]
     
-    for x in header:
-        para.append(str(x))
+    #for x in header:
+    #    para.append(str(x))
+    #print(para)
 
-    
     sp1 = [l.split('>')[1] for l in [l.split('</th>')[0] for l in para]]
 
 
-    detail = soup.find_all('tr')
-    row = []
-    for y in detail:
-        if str(y).find('<tr align="right"><td>') != -1: #and str(y).find('</td><td>') != -1 :
-            row.append(str(y))
-    row = row[1:]
-    sp2 = [l.split('</td>')[0] for l in [l.split('<tr align="right"><td>')[1] for l in row]]
-    sp3 = [l.split('<td>')[1] for l in [l.split('</td>')[1] for l in row]]
-    sp4 = [l.split('<td>')[1] for l in [l.split('</td>')[2] for l in row]]
-    sp5 = sp5 + list(zip(sp2,sp3))
-    sp5 = sp5 + list(zip(sp2,sp4))
-    final_temp = sorted(sp5,key=itemgetter(1))
+    #detail = soup.find_all(re.compile(r'<tr align="right"><td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>'))
+    detail = soup.find_all(re.compile(r"tr"))
+    detail1 = re.findall(r'<td>(\d+)</td><td>(\w+)</td>\<td>(\w+)</td>', str(detail))
+    #print(detail1)
+    #row = []
+    #row = [str(y) for y in detail if str(y).find('<tr align="right"><td>') != -1]
+    #for y in detail:
+    #    if str(y).find('<tr align="right"><td>') != -1: #and str(y).find('</td><td>') != -1 :
+    #        row.append(str(y))
+    #print(row)
+    #sys.exit(1)
+    #row = row[1:]
+    #sp2 = [l.split('</td>')[0] for l in [l.split('<tr align="right"><td>')[1] for l in row]]
+    #sp3 = [l.split('<td>')[1] for l in [l.split('</td>')[1] for l in row]]
+    #sp4 = [l.split('<td>')[1] for l in [l.split('</td>')[2] for l in row]]
+    #sp5 = sp5 + list(zip(sp2,sp3))
+    #sp5 = sp5 + list(zip(sp2,sp4))
+    #final_temp = sorted(sp5,key=itemgetter(1))
+    final_temp = sorted(detail1,key=itemgetter(1))
     final_temp = [l[1] + " " + l[0] for l in final_temp ]
     final = []
     for i in final_temp:
@@ -81,9 +89,13 @@ def extract_names(f):
                 final = final[:-1:]
             else:
                 final = final[:-2:] + final[-1:]
-    print(fil)
-    for i in final:
-        print(str(i))
+    #print(fil)
+
+    ab = '\n'.join(final)
+    #for i in final:
+    #print(final)
+    print(ab)
+
 
 
     return
@@ -95,32 +107,50 @@ def main():
     # which is the script itself.
     args = sys.argv[1:]
 
-    #if not args:
-    #  print 'usage: [--summaryfile] file [file ...]'
-    #  sys.exit(1)
+    if not args:
+      print('usage: [--summaryfile] file [file ...]')
+      sys.exit(1)
 
     # Notice the summary flag and remove it from args if it is present.
+    #print(args)
+    #print(args[1:])
     summary = False
-    #if args[0] == '--summaryfile':
-    #  summary = True
-    #  del args[0]
+    if args[0] == '--summaryfile':
+      summary = True
+      del args[0]
+    else:
+      print('usage: [--summaryfile] file [file ...]')
+      sys.exit(1)      
 
+    if not args:  
+      print('usage: [--summaryfile] file [file ...]')
+      sys.exit(1)
+        
+    #print(args[0])  
     # +++your code here+++
     # For each filename, get the names, then either print the text output
     # or write it to a summary file
+    
     #html = urllib2.urlopen(your_site_here)
     #soup = BS(html)
     #elem = soup.findAll('a', {'title': 'title here'})
     #elem[0].text
-    whole_files = [os.path.abspath(f) for f in os.scandir(os.getcwd() + '\\' + 'babynames\\') if f.is_file() if str(f.name).find(".html") != -1]
-    for f in whole_files:
+    #whole_files = [os.path.abspath(f) for f in os.scandir(os.getcwd() + '\\' + 'babynames\\') if f.is_file() if str(f.name).find(".html") != -1]
+    for f in args:
+        fil = os.getcwd() + '\\' + f
+        #print(fil)
         extract_names(f)
-        print("******************************")
+        break
+    #for f in whole_files:
+        #extract_names(f)
+    #    print("******************************")
+    #    break
 
     #print(whole_files)
             #content = file.read()
       #content1 =  [l.split('</td>\n') for l in content.split('<tr align="right"><td>') ]
       #print(content1[-10:-1])
+
 
 
 if __name__ == '__main__':
