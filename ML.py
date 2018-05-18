31b0f4b85dd0c48423a5362d5233874cc60cb95a
Dayalan <arasan_dt@yahoo.co.in>
2018-05-18 10:47:19 -0400

    Dont ML for today 5/18. Trained and Test the sample for accuracy

--- ML.py
+++ ML.py
@@ -1,6 +1,10 @@
 import pandas as pd
-import quandl
-import math
+import quandl, math
+import numpy as np
+import scipy
+#cross_validation as changed to model_selection
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 #print(df.head)
@@ -15,8 +19,31 @@ df.fillna(-99999,inplace=True)
 #forecast_out = int(math.ceil(0.01*len(df)))
 #forecast_out = (0.01*len(df))
 forecast_out = math.ceil(0.01*len(df))
-#print(forecast_out)
+print(forecast_out)
 #print(type(forecast_out))
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
-print(df.head())
+df.dropna(inplace=True) 
+#print(df.head())
+#X = Features
+#y - labels or output
+
+X = np.array(df.drop(['label'],axis = 1))
+X = preprocessing.scale(X)
+
+#X= X[:-forecast_out+1]
+#print(df[df.isnull().any(axis=1)])
+#df.dropna(inplace=True)
+#print(X)
+#make sure length of trest and train is same
+y=np.array(df['label'])
+#print(y)
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=0.2)
+
+clf = LinearRegression(n_jobs=-1)
+#clf =svm.SVR(kernel='poly')
+clf.fit(X_train,y_train)
+accuracy = clf.score(X_test,y_test)
+print(accuracy)
+
+
