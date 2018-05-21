b97b9b2860a504c3be675873b86d92848c30ee65
Dayalan <arasan_dt@yahoo.co.in>
2018-05-21 10:52:55 -0400

    ml and olympics. done for the day.

--- ML.py
+++ ML.py
@@ -1,10 +1,14 @@
 import pandas as pd
-import quandl, math
+import pickle
+import quandl, math, datetime,time
 import numpy as np
 import scipy
 #cross_validation as changed to model_selection
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
+#import matplotlib.pyplot as plt
+#from matplotlib.pyplot import style
+
 
 df = quandl.get('WIKI/GOOGL')
 #print(df.head)
@@ -22,28 +26,60 @@ forecast_out = math.ceil(0.01*len(df))
 print(forecast_out)
 #print(type(forecast_out))
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True) 
 #print(df.head())
 #X = Features
 #y - labels or output
 
 X = np.array(df.drop(['label'],axis = 1))
 X = preprocessing.scale(X)
-
+X_lately = X[-forecast_out:]
+#print(X_lately)
+X = X[:-forecast_out]
+#quit()
 #X= X[:-forecast_out+1]
 #print(df[df.isnull().any(axis=1)])
 #df.dropna(inplace=True)
 #print(X)
 #make sure length of trest and train is same
+df.dropna(inplace=True) 
+
 y=np.array(df['label'])
 #print(y)
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=0.2)
 
-clf = LinearRegression(n_jobs=-1)
-#clf =svm.SVR(kernel='poly')
-clf.fit(X_train,y_train)
+# clf = LinearRegression(n_jobs=-1)
+# #clf =svm.SVR(kernel='poly')
+# clf.fit(X_train,y_train)
+# with open('./input/linerregression.pickle','wb') as f:
+# 	pickle.dump(clf,f)
+pickle_in = open('./input/linerregression.pickle','rb')
+clf = pickle.load(pickle_in)
 accuracy = clf.score(X_test,y_test)
-print(accuracy)
+#print(accuracy)
+
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
 
 
+df['Forecast'] = np.nan
+#print(df.tail())
+
+last_date = df.iloc[-1].name
+#print(last_date)
+#last_unix = last_date.timestamp()
+last_unix = time.mktime(datetime.datetime.strptime(str(last_date), "%Y-%m-%d %H:%M:%S").timetuple())
+#print(last_unix1)
+#print(last_unix)
+one_day = 86400
+next_unix = last_unix + one_day
+#next_date = datetime.datetime.fromtimestamp(next_unix)
+#print(next_date)
+for i in forecast_set:
+	next_date = datetime.datetime.fromtimestamp(next_unix)
+	#print(next_date)
+	next_unix += one_day
+	df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+print(df.tail(40))
+df.to_pickle("./input/GOOGL.pkl")
--- Olympics.py
+++ Olympics.py
@@ -17,20 +17,70 @@ medalists = pd.read_csv('./input/Summer Olympic medalists 1896 to 2008 - ALL MED
 countrycodes = pd.read_csv('./input/Summer Olympic medalists 1896 to 2008 - IOC COUNTRY CODES.csv', delimiter=',')
 #print(editions.head())
 #print(medalists.head())
+#print(medalists.loc[medalists['NOC']== 'YUG'])
+#quit()
 #print(medalists.shape)
 #print(countrycodes.head())
 medalibyyearcountry = medalists.pivot_table(index=['Edition'],columns='NOC', values=['Sport'],aggfunc='count').fillna('0')
-#medalibyyearcountry.divide(medalibyyearcountry.sum)
-#print(medalibyyearcountry)
-mean_fractions = medalibyyearcountry.expanding().mean().reset_index()
+#print(medalibyyearcountry.head())
+print(medalibyyearcountry[('Sport','ZZX')])
+#quit()
+#medalibyyearcountry.reset_index()
+medalibyyearcountry.columns = medalibyyearcountry.columns.droplevel()
+#print(medalibyyearcountry.head())
+#medalibyyearcountry1 = medalibyyearcountry.iloc[0:,1:].astype(float).pct_change()
+medalibyyearcountry = medalibyyearcountry.astype(float).pct_change()
+#del medalibyyearcountry1['Edition']
+#medalibyyearcountry1['Edit'] = medalibyyearcountry['Edition']
+#medalibyyearcountry = medalibyyearcountry1
+#print(medalibyyearcountry.head())
+#print(medalibyyearcountry.columns)
+#print(medalibyyearcountry.shape)
+COLUMN_NAMES = ['NOC','Change','Edition']
+medalibyyearco = pd.DataFrame(columns=COLUMN_NAMES)
+for i in range(len(medalibyyearcountry)):
+	a = medalibyyearcountry.iloc[i].to_frame().reset_index()
+	a['Edition'] = a.columns.values.tolist()[1] #int(a.iloc[-1][2])
+	a.columns = COLUMN_NAMES
+	medalibyyearco = medalibyyearco.append(a,ignore_index=True)
+	#print(medalibyyearco)
+	#break
+	
+	#print(a)
+	#print(a.columns)
+	#print(medalibyyearcountry.iloc[i].to_frame().transpose())
+	#pass #print(medalibyyearcountry.iloc[i].name)
+	#medalibyyearco['Edition'] = medalibyyearcountry['Edition']   #[np.nan for _ in range(len(medalibyyearcountry.columns) - 1)] 
+#medalibyyearco = medalibyyearco[['Edition','NOC','Change']]
+#print(type(medalibyyearco))
+medalibyyearco.sort_values(by=['NOC','Edition'],inplace=True)
+medalibyyearco.reset_index(inplace=True,drop=True)
+medalibyyearco = medalibyyearco[["Edition","NOC","Change"]] 
+#print(medalibyyearco.columns)
+#print(medalibyyearco.head())
+print(medalibyyearco.loc[medalibyyearco['NOC'] == 'ZZX'])
+#print(medalibyyearcountry.iloc[0])
+#print(medalibyyearcountry.iloc[:][0])
+
+#print(medalibyyearcountry[('Sport','AFG')])
+#a = medalibyyearcountry[('Sport','USA')].to_frame()#.reset_index(0)
+#a.columns = ['Edition', 'Medals']
+#a.reset_index(level='Edition',col_level=0)
+#a.set_index('Edition')
+#a['Percent'] = a.astype(float).pct_change() * 100
+#print(a)
+#print(type(a))
+#print(a.columns)
+#print(medalibyyearcountry.columns)
+#mean_fractions = medalibyyearcountry.expanding().mean().reset_index()
 #print(mean_fractions)
 #print(mean_fractions.columns)
 #print(mean_fractions.loc[:][('Sport','USA')])
 # #print(mean_fractions.loc[:,'']
-a = mean_fractions.loc[:][('Sport','USA')].reset_index()
-fractions_change = a.pct_change() * 100
+#a = mean_fractions.loc[:][('Sport','USA')].reset_index()
+#fractions_change = a.pct_change() * 100
 #print(fractions_change.columns)
-b = fractions_change.loc[:][('Sport','USA')].reset_index()
+#b = fractions_change.loc[:][('Sport','USA')].reset_index()
 #print(a)
 #print(b)
-print(pd.merge(a,b,how='inner',on=['index']))
+#print(pd.merge(a,b,how='inner',on=['index']))
