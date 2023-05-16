5518cc359f33f265d5b5045b4767aa4bca072153
Arasan Dayalan <thiru.arasan.dayalan@accenture.com>
2023-05-16 05:39:58 +0000

    minorchange

--- cognito.py
+++ cognito.py
@@ -1,7 +1,7 @@
 import requests
 
 CLIENT_ID = "4sinvc6tuu8rf942ehblblabp01"
-CLIENT_SECRET = "1f2b4r9ishrrkgd1511f4pb9j4k95upn7docg38ib3pjfchhvn7e"
+CLIENT_SECRET = ""
 COGNITO_TOKEN_ENDPOINT = (
     "https://meaintegration.auth.us-east-2.amazoncognito.com/oauth2/token"
 )
